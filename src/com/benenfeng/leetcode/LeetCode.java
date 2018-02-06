aa07fa14a8d163daa933c8d65ea212a0faf7818e
Benenfeng <benenfeng@126.com>
2018-02-06 22:47:41 +0800

    leetcode: push google check style

--- src/com/benenfeng/leetcode/LeetCode.java
+++ src/com/benenfeng/leetcode/LeetCode.java
@@ -3,7 +3,9 @@ package com.benenfeng.leetcode;
 public class LeetCode {
 
   public static void main(String arg[]) {
-    System.out.println("this is leetcode class for test github!");
+     System.out.println("this is leetcode class for test github!");
+     System.out.println("hello world!");
+     System.out.println("hhh");
   }
 
 }
